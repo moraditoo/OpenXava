a113fe894c0933d8147f7a669978b287d8e17de2
Dylan Mora <damora@uamv.edu.ni>
2026-06-04 22:37:37 -0600

    correcion a rebuild de maven para que se conecte a la db

--- facturacion/src/main/java/ni/edu/uam/facturacion/run/facturacion.java
+++ facturacion/src/main/java/ni/edu/uam/facturacion/run/facturacion.java
@@ -9,8 +9,8 @@ import org.openxava.util.*;
 public class facturacion {
 
 	public static void main(String[] args) throws Exception {
-		//DBServer.start("facturacion-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
-		AppServer.run("facturacion"); // Usa AppServer.run("") para funcionar en el contexto raíz
+		// DBServer.start("miaplicacion-db"); // Comenta o elimina, HSQLDB ya no es necesario
+		AppServer.run("facturacion");
 	}
 
 }
